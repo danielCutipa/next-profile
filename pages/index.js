97d586af441462c5ce210b51cd071be6d4ae9792
Daniel Cutipa <daniel.cutipa@mensivo.com>
2021-01-22 04:45:11 -0400

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 // import * as React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import Link from '@material-ui/core/Link'
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary">
+    <Typography variant='body2' color='textSecondary'>
       {'Copyright © '}
-      <Link color="inherit" href="https://danielcutipa.com/">
+      <Link color='inherit' href='https://danielcutipa.com/'>
         Daniel Cutipa
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 export default function StickyFooter() {
@@ -24,22 +24,22 @@ export default function StickyFooter() {
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        minHeight: '100vh',
+        minHeight: '100vh'
       }}
     >
       <CssBaseline />
-      <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
+      <Container component='main' sx={{ mt: 8, mb: 2 }} maxWidth='sm'>
+        <Typography variant='h2' component='h1' gutterBottom>
+          Daniel Cutipa
         </Typography>
-        <Typography variant="h5" component="h2" gutterBottom>
-          {'Pin a footer to the bottom of the viewport.'}
-          {'The footer will move as the main element of the page grows.'}
+        <Typography variant='h5' component='h2' gutterBottom>
+          {'Frontend Developer with React js.'}
+          {'Team Leader and Coach.'}
         </Typography>
-        <Typography variant="body1">Sticky footer placeholder.</Typography>
+        <Typography variant='body1'>That is a test.</Typography>
       </Container>
       <Box
-        component="footer"
+        component='footer'
         sx={{
           py: 3,
           px: 2,
@@ -47,16 +47,16 @@ export default function StickyFooter() {
           backgroundColor: (theme) =>
             theme.palette.mode === 'light'
               ? theme.palette.grey[200]
-              : theme.palette.grey[800],
+              : theme.palette.grey[800]
         }}
       >
-        <Container maxWidth="sm">
-          <Typography variant="body1">
+        <Container maxWidth='sm'>
+          <Typography variant='body1'>
             My sticky footer can be found here.
           </Typography>
           <Copyright />
         </Container>
       </Box>
     </Box>
-  );
+  )
 }
