c445b0975ae32d079419e2f78e9bed5ba62f415f
Daniel Cutipa <daniel.cutipa@mensivo.com>
2021-01-23 03:57:31 -0400

    change to other project

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,17 @@
-import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
-import Footer from '../components/Footer'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import ProTip from '../src/ProTip'
+import Link from '../src/Link'
+import Copyright from '../src/Copyright'
 
-const Home = () => {
+const Index = () => {
   return (
-    <Box
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh'
-      }}
+    <Container
+      maxWidth='sm'
+      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
     >
-      <Container component='main' maxWidth='md' style={{ paddingTop: '30vh' }}>
+      <Box style={{ marginTop: '20vh' }}>
         <Typography variant='h2' component='h1' gutterBottom>
           Daniel Cutipa
         </Typography>
@@ -22,11 +21,36 @@ const Home = () => {
         <Typography variant='h5' component='h2' gutterBottom>
           {'Team Leader and Coach.'}
         </Typography>
-        <Typography variant='body1'>{`</>`}</Typography>
-      </Container>
-      <Footer />
-    </Box>
+        <Link href='/about' color='secondary'>
+          Go to the about page
+        </Link>
+        <ProTip />
+      </Box>
+      <Copyright />
+    </Container>
   )
 }
 
-export default Home
+export default Index
+
+// <Box
+//       style={{
+//         display: 'flex',
+//         flexDirection: 'column',
+//         minHeight: '100vh'
+//       }}
+//     >
+//       <Container component='main' maxWidth='md' style={{ paddingTop: '30vh' }}>
+//         <Typography variant='h2' component='h1' gutterBottom>
+//           Daniel Cutipa
+//         </Typography>
+//         <Typography variant='h4' component='h2' gutterBottom>
+//           {'Frontend Developer with React js.'}
+//         </Typography>
+//         <Typography variant='h5' component='h2' gutterBottom>
+//           {'Team Leader and Coach.'}
+//         </Typography>
+//         <Typography variant='body1'>{`</>`}</Typography>
+//       </Container>
+//       <Footer />
+//     </Box>
