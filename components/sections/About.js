2a3a1656835fddfff49cb969f2d2cf8a6378a41f
Daniel Cutipa <daniel.cutipa@mensivo.com>
2021-01-27 02:46:57 -0400

    add some features in my profile

--- components/sections/About.js
+++ components/sections/About.js
@@ -1,26 +1,61 @@
-import { Box, Paper, Typography } from '@material-ui/core'
+import {
+  Box,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  Button
+} from '@material-ui/core'
 import styles from 'styles/About.module.css'
-import BuildIcon from '@material-ui/icons/Build'
+import { makeStyles } from '@material-ui/core/styles'
+import CardMedia from '@material-ui/core/CardMedia'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  media: {
+    height: 300
+  }
+}))
 
 const About = () => {
+  const classes = useStyles()
+
   return (
     <Box component='section' color='secondary' className={styles.wrap}>
       <Typography variant='h2' align='center' sx={{ mt: 2 }} gutterBottom>
         About me
       </Typography>
-      <Typography
-        variant='h3'
-        align='center'
-        color='secondary'
-        sx={{ mt: '10vh' }}
-        gutterBottom
-      >
-        Building <BuildIcon />
-      </Typography>
-      {/* <div className={styles.root}>
-        <Paper className={styles.paper} elevation={15} />
-        <Paper className={styles.paper} elevation={24} />
-      </div> */}
+
+      <Container maxWidth='md'>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item sm={6} xs={12} align='center'>
+              <img
+                src='/src/images/photo.jpg'
+                className={styles.img}
+                alt='photo'
+              />
+            </Grid>
+            <Grid item sm={6} xs={12} className={styles.text}>
+              <Box>
+                <Typography variant='h5' gutterBottom component='div'>
+                  Text about me
+                </Typography>
+                <Button variant='contained' color='secondary'>
+                  Resume
+                </Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
     </Box>
   )
 }
--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -1,26 +1,70 @@
-import { Box, Paper, Typography } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  makeStyles,
+  Typography
+} from '@material-ui/core'
 import styles from 'styles/Projects.module.css'
-import BuildIcon from '@material-ui/icons/Build'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary
+  },
+  media: {
+    height: 300
+  }
+}))
 
 const About = () => {
+  const classes = useStyles()
+
   return (
     <Box component='section' color='secondary' className={styles.wrap}>
       <Typography variant='h2' align='center' sx={{ mt: 2 }} gutterBottom>
         Projects
       </Typography>
-      <Typography
-        variant='h3'
-        align='center'
-        color='secondary'
-        sx={{ mt: '10vh' }}
-        gutterBottom
-      >
-        Building <BuildIcon />
-      </Typography>
-      {/* <div className={styles.root}>
-        <Paper className={styles.paper} elevation={15} />
-        <Paper className={styles.paper} elevation={24} />
-      </div> */}
+
+      <Container maxWidth='md'>
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item sm={5} xs={12}>
+              <Box>
+                <Typography variant='h4' gutterBottom component='div'>
+                  ThinkShopStore
+                </Typography>
+                <Typography variant='body1' gutterBottom>
+                  body1. Lorem ipsum dolor sit amet, consectetur adipisicing
+                  elit. Quos blanditiis tenetur unde suscipit, quam beatae rerum
+                  inventore consectetur, neque doloribus, cupiditate numquam
+                  dignissimos laborum fugiat deleniti? Eum quasi quidem
+                  quibusdam.
+                </Typography>
+                <Link href='https://thinkshopstore.com/' target='_blank'>
+                  <Button variant='outlined' color='secondary'>
+                    See live
+                  </Button>
+                </Link>
+              </Box>
+            </Grid>
+
+            <Grid item sm={7} xs={12} align='center'>
+              <img
+                src='/src/images/think.PNG'
+                className={styles.img}
+                alt='photo'
+              />
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
     </Box>
   )
 }
