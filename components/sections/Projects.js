9080d2498953b291d7b569a94d0931739c88df50
Daniel Cutipa <daniel.cutipa@mensivo.com>
2021-02-03 02:34:16 -0400

    other project was added

--- components/sections/Projects.js
+++ components/sections/Projects.js
@@ -7,6 +7,7 @@ import {
   makeStyles,
   Typography
 } from '@material-ui/core'
+import Project from 'components/Project'
 import styles from 'styles/Projects.module.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -34,40 +35,25 @@ const About = () => {
 
       <Container maxWidth='md'>
         <div className={classes.root}>
-          <Grid container spacing={3}>
-            <Grid item sm={5} xs={12}>
-              <Box>
-                <Typography variant='h4' gutterBottom component='div'>
-                  ThinkShopStore
-                </Typography>
-                <Typography variant='body1' gutterBottom>
-                  ThinkShop is a web application for the sale of real state such
-                  as apartments, it's build with{' '}
-                  <Link href='https://www.odoo.com' target='_blank'>
-                    Odoo
-                  </Link>{' '}
-                  taking advantage of the tools it provides.
-                </Typography>
-
-                <Button
-                  variant='outlined'
-                  color='secondary'
-                  href='https://thinkshopstore.com/'
-                  target='_blank'
-                >
-                  See live
-                </Button>
-              </Box>
-            </Grid>
-
-            <Grid item sm={7} xs={12} align='center'>
-              <img
-                src='/src/images/think.PNG'
-                className={styles.img}
-                alt='photo'
-              />
-            </Grid>
-          </Grid>
+          <Project
+            title={'ThinkShopStore'}
+            // description={`ThinkShop is a web application for the sale of real state such as apartments, it's build with Odoo taking advantage of the tools it provides.`}
+            description={`ThinkShop is a web application for the sale of real state such as
+            apartments, it's build with`}
+            description2={'taking advantage of the tools it provides.'}
+            url={'https://thinkshopstore.com/'}
+            techUrl={'https://www.odoo.com'}
+            tech={'Odoo'}
+            image={'/src/images/think.PNG'}
+          />
+          <Project
+            title={'Universiclick'}
+            description={
+              'Universiclick is one of the best platform for managing students and teachers of an educational institution.'
+            }
+            url={'https://universiclick.com/'}
+            image={'/src/images/universiclick.PNG'}
+          />
         </div>
       </Container>
     </Box>
